5ff3fdae51069fbd3f4fea4eef2510e7692f4d7d
VincentK <vincent.kesumo@gmail.com>
2017-11-29 12:16:49 +0100

    Disabled session store, and added seq.sync

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE_URL);
+sequelize.sync();
 
 const User = sequelize.import('./user.js');
 const Topic = sequelize.import('./topic.js');
