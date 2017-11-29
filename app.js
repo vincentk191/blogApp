24fe723242369d49345a5238c3df19b398ae34bf
VincentK <vincent.kesumo@gmail.com>
2017-11-29 11:21:45 +0100

    Changed port ID?

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const model = require('./models');
 
 //-----------------CONFIGURATION------------------
 const app = express();
-const portID = process.env.PORT || 3000;
+const portID = process.env.PORT || 5000;
 
 app.use(bodyParser.urlencoded({
    extended: false
@@ -33,5 +33,5 @@ app.use(express.static('public'));
 app.use(require('./routes'));
 
 app.listen(portID, () => {
-   console.log(`Server's working just fine on port 3000!`);
+   console.log(`Server's working just fine on port 5000!`);
 });
