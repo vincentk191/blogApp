7b0b6a370441106f5e82741cff77e23e99a51bbb
VincentK <vincent.kesumo@gmail.com>
2017-11-29 12:13:51 +0100

    Remove session store

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const Sequelize = require('sequelize');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const model = require('./models');
 
 //-----------------CONFIGURATION------------------
@@ -17,12 +17,12 @@ app.use(bodyParser.urlencoded({
 app.use(session({
    secret: "safe",
    saveUninitialized: true,
-   resave: false,
-   store: new SequelizeStore({
-      db: model.sequelize,
-      checkExpirationInterval: 15 * 60 * 1000, // The interval at which to cleanup expired sessions in milliseconds.
-      expiration: 60 * 60 * 1000 // The maximum age (in milliseconds) of a valid session.
-   })
+   resave: false
+   // store: new SequelizeStore({
+   //    db: model.sequelize,
+   //    checkExpirationInterval: 15 * 60 * 1000, // The interval at which to cleanup expired sessions in milliseconds.
+   //    expiration: 60 * 60 * 1000 // The maximum age (in milliseconds) of a valid session.
+   // })
 }))
 //-------------------------------------------------
 
